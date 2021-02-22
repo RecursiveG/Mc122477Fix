6f4ab2ae792d52e8fc5480f006e2f676b9c2258e
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2021-02-22 02:24:40 -0600

    Backport to 1.14

--- src/main/java/me/sizableshrimp/mc122477fix/mixin/MixinMinecraftClient.java
+++ src/main/java/me/sizableshrimp/mc122477fix/mixin/MixinMinecraftClient.java
@@ -1,16 +1,16 @@
 package me.sizableshrimp.mc122477fix.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import me.sizableshrimp.mc122477fix.GLFWPollCallback;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(RenderSystem.class)
-public class MixinRenderSystem {
-    @Inject(method = "flipFrame", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwPollEvents()V", shift = At.Shift.AFTER))
-    private static void injectGlfwPoll(CallbackInfo ci) {
+@Mixin(MinecraftClient.class)
+public class MixinMinecraftClient {
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GLX;pollEvents()V", shift = At.Shift.AFTER, ordinal = 0))
+    private void injectGlfwPoll(CallbackInfo ci) {
         GLFWPollCallback.EVENT.invoker().onPoll();
     }
 }
