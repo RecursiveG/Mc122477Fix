3d078d1bca248d508c41455c5aa3a03fabdaae3c
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2021-02-22 00:07:16 -0600

    Update to 1.1 with bugfixes and improvements
    The mod now cancels all char type events when opening the chat.
    The detection is now based on event poll counts instead of time intervals.

--- src/main/java/me/sizableshrimp/mc122477fix/Mc122477Fix.java
+++ src/main/java/me/sizableshrimp/mc122477fix/Mc122477Fix.java
@@ -3,28 +3,45 @@ package me.sizableshrimp.mc122477fix;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.ActionResult;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class Mc122477Fix implements ClientModInitializer {
-    private long chatOpenMillis;
+    private long pollCount;
+    private long prevKeyPoll;
 
     @Override
     public void onInitializeClient() {
-        GuiOpenCallback.EVENT.register(screen -> {
-            if (screen instanceof ChatScreen)
-                this.chatOpenMillis = System.currentTimeMillis();
+        GLFWPollCallback.EVENT.register(() -> this.pollCount++);
 
-            return ActionResult.PASS;
-        });
-        GuiCharTypedCallback.EVENT.register((chr, modifiers) -> {
-            if (System.currentTimeMillis() - this.chatOpenMillis < 50) {
-                // If less than 50ms has occurred since the chat box has opened, cancel to fix GLFW/glfw#1794 on Unix-based systems.
-                return ActionResult.FAIL;
+        // Key press events are always processed before char type events
+        KeyboardKeyPressedCallback.EVENT.register((window, key, scancode, action, modifiers) -> {
+            MinecraftClient client = MinecraftClient.getInstance();
+            // If this is a key release/repeat OR we're already in a screen (including chat screen), skip
+            if (action != GLFW.GLFW_PRESS || client.currentScreen != null)
+                return ActionResult.PASS;
+
+            // If the chat or command key was pressed, store what poll count it happened on.
+            if (client.options.keyChat.matchesKey(key, scancode) || client.options.keyCommand.matchesKey(key, scancode)) {
+                this.prevKeyPoll = this.pollCount;
+            } else {
+                // Otherwise, set to -1
+                this.prevKeyPoll = -1L;
             }
 
             return ActionResult.PASS;
+        });
+        KeyboardCharTypedCallback.EVENT.register((window, codepoint, modifiers) -> {
+            // If the previous key poll is -1 or the poll count doesn't match up closely, skip
+            if (this.prevKeyPoll == -1 || this.pollCount - this.prevKeyPoll > 5)
+                return ActionResult.PASS;
+
+            // If we are on a close poll count to when the key press event was polled,
+            // then we should cancel the char type event to ensure it is not passed to the chat field.
+            this.prevKeyPoll = -1;
+            return ActionResult.FAIL;
         });
     }
 }
