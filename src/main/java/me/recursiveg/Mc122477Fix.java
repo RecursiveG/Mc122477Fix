8d9ae6ed202fbb36b4242a8db977a7fea9b40aa9
Recursive G <g.recursive@gmail.com>
2021-12-01 21:16:24 -0600

    update to mc 1.18

--- src/main/java/me/recursiveg/Mc122477Fix.java
+++ src/main/java/me/recursiveg/Mc122477Fix.java
@@ -2,8 +2,8 @@ package me.recursiveg;
 
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
-import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,12 +17,12 @@ public class Mc122477Fix
 
     long lastScreenOpenTime = 0;
     @SubscribeEvent
-    public void onScreenOpen(GuiOpenEvent ev) {
-        if (ev.getGui() instanceof ChatScreen || ev.getGui() instanceof CreativeModeInventoryScreen) lastScreenOpenTime = System.currentTimeMillis();
+    public void onScreenOpen(ScreenOpenEvent ev) {
+        if (ev.getScreen() instanceof ChatScreen || ev.getScreen() instanceof CreativeModeInventoryScreen) lastScreenOpenTime = System.currentTimeMillis();
     }
 
     @SubscribeEvent
-    public void onCharTyped(GuiScreenEvent.KeyboardCharTypedEvent.Pre ev) {
+    public void onCharTyped(ScreenEvent.KeyboardCharTypedEvent.Pre ev) {
         if (System.currentTimeMillis() - lastScreenOpenTime < 50) ev.setCanceled(true);
     }
 }
