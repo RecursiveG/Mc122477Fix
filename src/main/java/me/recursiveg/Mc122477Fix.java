082dc2b096d4c42817dcebcb4ed0463619788e86
Recursive G <g.recursive@gmail.com>
2022-08-13 22:00:12 -0500

    update code for 1.19.2

--- src/main/java/me/recursiveg/Mc122477Fix.java
+++ src/main/java/me/recursiveg/Mc122477Fix.java
@@ -3,7 +3,6 @@ package me.recursiveg;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,14 +17,21 @@ public class Mc122477Fix {
     long renderTicksSinceScreenOpen = 0;
 
     @SubscribeEvent
-    public void onScreenOpen(ScreenOpenEvent ev) {
-        if (ev.getScreen() instanceof ChatScreen || ev.getScreen() instanceof CreativeModeInventoryScreen) {
+    public void onScreenOpen(ScreenEvent.Opening ev) {
+        if (ev.getNewScreen() instanceof ChatScreen || ev.getNewScreen() instanceof CreativeModeInventoryScreen) {
             renderTicksSinceScreenOpen = 0;
         }
     }
 
     @SubscribeEvent
-    public void onCharTyped(ScreenEvent.KeyboardCharTypedEvent.Pre ev) {
+    public void onKeyPressed(ScreenEvent.KeyPressed.Pre ev) {
+        if (renderTicksSinceScreenOpen < 2) {
+            ev.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onCharTyped(ScreenEvent.CharacterTyped.Pre ev) {
         if (renderTicksSinceScreenOpen < 2) {
             ev.setCanceled(true);
         }
@@ -33,6 +39,8 @@ public class Mc122477Fix {
 
     @SubscribeEvent
     public void onPostClientTick(TickEvent.RenderTickEvent ev) {
-        if (ev.phase == TickEvent.Phase.END) renderTicksSinceScreenOpen++;
+        if (ev.phase == TickEvent.Phase.END) {
+            renderTicksSinceScreenOpen++;
+        }
     }
 }
